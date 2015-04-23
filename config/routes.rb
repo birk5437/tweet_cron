0acefd41939e7a0b1748cb720c918b668beebf9e
Burke Ramsey <birk5437@gmail.com>
2015-04-23 16:04:28 -0400

    change root route

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ end
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
 
-  root 'items#index'
+  root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
